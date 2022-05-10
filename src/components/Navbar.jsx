33af331b879fcc98ac101542ce2ba33fb07644ef
Aaron <aaron.vmale@gmail.com>
2022-05-09 20:08:20 -0700

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link, useNavigate } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className="navbarWrapper">
+        <div className="navbarTitle">Aaron Van Der Male</div>
+        <nav className="navbar">
+            <ul>
+                <li><Link to='/'>Education & Experience</Link ></li>
+                <li><Link to='/'>Projects</Link ></li>
+            </ul>
+        </nav>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/components/Projects.jsx
@@ -0,0 +1,79 @@
+import { FaChess } from 'react-icons/fa'
+import { MdOutlineDashboardCustomize } from 'react-icons/md'
+const Projects = () => {
+  return (
+    <>
+      <div className="main">
+        <div className="NameContainer">
+          <h1 className="Name">Projects</h1>
+        </div>
+      
+      <div className="projectsWrapper">
+        <div className="Subheader1">
+          <div className="Subheader1Label"><MdOutlineDashboardCustomize /> Wordle Clone</div>
+          <div className="Subheader2Label">Northeastern University</div>
+          <div className="Subheader2Right">March, 2022</div>
+          <div className="Subheader3Content">
+            <ul>
+              <li>A clone of <em>Wordle</em> using React and Redux </li>
+              <li>Custom CSS</li>
+              <li><a href="https://av-wordle.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live Demo</a></li>
+              <li><a href="https://github.com/vmalecode/aaron-vandermale-project2-wordle" target="_blank" rel="noopener noreferrer">Source</a></li>
+              <div className="imgWrap">
+                <img src={require('./wordl.gif')} alt="wordl.gif"></img>
+              </div>
+            </ul>
+          </div>
+        </div>
+        <div className="Subheader1">
+          <div className="Subheader1Label"><FaChess /> Chess </div>
+          <div className="Subheader2Label">The Odin Project Bootcamp</div>
+          <div className="Subheader2Right">January, 2021</div>
+          <div className="Subheader3Content">
+            <ul>
+              <li>A functional game of chess in the CLI</li>
+              <li>Utilize OOD principles in Ruby</li>
+              <li><a href="https://github.com/vmalecode/chess" target="_blank" rel="noopener noreferrer">Link to source</a></li>
+              <li><a href="https://replit.com/@vmalecode/Ruby-Chess-REPL#main.rb" target="_blank" rel="noopener noreferrer">Live Demo</a></li>
+              <div className="imgWrap">
+                <img src={require('./chess.gif')} alt="chess.gif" id='chess'></img>
+              </div>
+            </ul>
+          </div>
+        </div>
+
+        <div className="Subheader1">
+          <div className="Subheader1Label">Connect Four</div>
+          <div className="Subheader2Label">Northeastern University</div>
+          <div className="Subheader2Right">May, 2021</div>
+          <div className="Subheader3Content">
+            <ul>
+              <li>Created Connect Four game python, efficiently utilizing classNamees and inheritance</li>
+              <li>Designed AI opponent that analyzes the game state up to six moves in the future</li>
+              <li>Animated falling disks with realistic kinematic model</li>
+              <li>Source available on request</li>
+            </ul>
+          </div>
+        </div>
+        <div className="Subheader1">
+          <div className="Subheader1Label">Anagrams</div>
+          <div className="Subheader2Label">Northeastern University</div>
+          <div className="Subheader3Content">
+            <ul>
+              <li>C program that finds all English anagrams</li>
+              <li>Uses hash table and singly linked list to efficiently return results</li>
+              <li>Runs in 3s</li>
+              <li><a href="https://github.com/vmalecode/projects/blob/main/anagramsProject.md" target="_blank" rel="noopener noreferrer">Program example execution, documentation</a></li>
+              <li>No memory leaks</li>
+              <li>Source available on request</li>
+            </ul>
+
+          </div>
+        </div>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default Projects
