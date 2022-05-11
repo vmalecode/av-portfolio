4785007442e96e63e220796c7e9fb2f1422c6fca
Aaron <aaron.vmale@gmail.com>
2022-05-10 18:40:26 -0700

    delete file

--- src/components/bar.jsx
+++
@@ -1,6 +0,0 @@
-<span data-view-component="true" class="Progress">
-    <span style="background-color:#f1e05a !important;;width: 85.4%;" itemprop="keywords" aria-label="JavaScript 85.4" data-view-component="true" class="Progress-item color-bg-success-emphasis"></span>
-    <span style="background-color:#563d7c !important;;width: 11.7%;" itemprop="keywords" aria-label="CSS 11.7" data-view-component="true" class="Progress-item color-bg-success-emphasis"></span>
-    <span style="background-color:#e34c26 !important;;width: 2.9%;" itemprop="keywords" aria-label="HTML 2.9" data-view-component="true" class="Progress-item color-bg-success-emphasis"></span>
-</span>
-html body.logged-in.env-production.page-responsive div.application-main div main#js-repo-pjax-container div#repo-content-pjax-container.repository-content div.clearfix.container-xl.px-3.px-md-4.px-lg-5.mt-4 div div.Layout.Layout--flowRow-until-md.Layout--sidebarPosition-end.Layout--sidebarPosition-flowRow-end div.Layout-sidebar div.BorderGrid.BorderGrid--spacious div.BorderGrid-row div.BorderGrid-cell div.mb-2 span.Progress
