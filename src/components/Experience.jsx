1a088d4b3fa0ede117142763694522c164984909
Aaron <aaron.vmale@gmail.com>
2023-08-28 18:08:29 -0700

    update styles, experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -18,9 +18,17 @@ const Experience = () => {
       <div className="Subheader1">
         <div className="Subheader1Label">Experience</div>
         <div className="Subheader2Label">The Boeing Company</div>
-        <div className="Subheader2Right">May 2018 – October 2020</div>
-        <div className="Subheader2Label">Propulsion Engineer</div>
         <div className="Subheader2Right">Everett, Washington</div>
+
+        <div className="Subheader2Label">Manufacturing Engineer (Boeing 777-9)</div>
+        <div className="Subheader2Right">June 2023 - Current</div>
+        <div className="Subheader3Content">
+          <ul>
+            <li>Developed model to analyze stack up tolerance in composite wing and compute shimming solution to reduce production flow of 777-9 wings by 12 manufacturing days</li>
+          </ul>
+        </div>
+        <div className="Subheader2Label">Propulsion Engineer	(Boeing 777-9)</div>
+        <div className="Subheader2Right">February 2020 - October 2020</div>
         <div className="Subheader3Content">
           <ul>
             <li>Automated business processes and eliminated data entry tasks in various web apps with VBA, saving hundreds of engineering hours while improving accuracy and correctness</li>
@@ -30,26 +38,14 @@ const Experience = () => {
           </ul>
         </div>
 
-        <div className="Subheader2Label">Manufacturing Engineer</div>
-        <div className="Subheader2Right">Everett, Washington</div>
+        <div className="Subheader2Label">Manufacturing Engineer (Boeing 767-2C)</div>
+        <div className="Subheader2Right">May 2018 - February 2020</div>
         <div className="Subheader3Content">
           <ul>
             <li>Project manager for projects involving multiple commodity owners, engineering teams, suppliers</li>
             <li>Managed cost and improvement projects for the 41/43 section of the 767-2C / KC-46 Tanker involving multiple organizations including tooling, manufacturing, design engineering
             </li>
             <li>Designed, tested, and implemented new tooling and tooling revisions</li>
-          </ul>
-        </div>
-        <div className="Subheader2Label">Pioneer Systems</div>
-        <div className="Subheader2Right">May 2017 – May 2018</div>
-        <div className="Subheader2Label">Mechanical Engineer</div>
-        <div className="Subheader2Right"></div>
-        <div className="Subheader3Content">
-          <ul>
-            <li>Responsible for all mechanical design and product development of bulk conveyance machinery</li>
-            <li>Integrate efforts from fabrication, assembly, purchasing teams</li>
-            <li>Ensure compliance with customer requirements, state, local, federal regulations</li>
-            <li>Responsible for all simulation to characterize vibrations, stress, fatigue, machine performance</li>
           </ul>
         </div>
       </div>
